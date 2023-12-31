9826bf791f15c2684d651a4a32df2e68da9b049c
tonyykosseifyy <tonykosseify123@gmail.com>
2023-12-31 16:35:44 +0200

    delete: testing student module

--- src/student/interface/student.interface.ts
+++
@@ -1,9 +0,0 @@
-import { Document } from 'mongoose';
-
-export interface IStudent extends Document { 
-    readonly name: string;
-    readonly roleNumber: number;
-    readonly class: number;
-    readonly gender: string;
-    readonly marks: number;
-}
