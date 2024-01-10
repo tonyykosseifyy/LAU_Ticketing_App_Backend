7a9314e4af1b4fab7d695511565de59ed8e4ed0c
tonyykosseifyy <tonykosseify123@gmail.com>
2024-01-10 16:37:36 +0200

    remove: scans modukle

--- src/scans/scans.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { ScansService } from './scans.service';
-import { ScansController } from './scans.controller';
-
-@Module({
-  providers: [ScansService],
-  controllers: [ScansController]
-})
-export class ScansModule {}
