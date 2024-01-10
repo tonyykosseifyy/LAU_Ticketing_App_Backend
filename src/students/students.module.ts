57af05edb814bf687ee3f6750ac6de3f3597b9d5
tonyykosseifyy <tonykosseify123@gmail.com>
2024-01-10 16:30:09 +0200

    register: student schema in student module

--- src/students/students.module.ts
+++ src/students/students.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { StudentsController } from './students.controller';
 import { StudentsService } from './students.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { StudentSchema } from './schemas/student.schema';
 
 @Module({
+  imports: [MongooseModule.forFeature([{ name: 'Student', schema: StudentSchema }])],
   controllers: [StudentsController],
   providers: [StudentsService]
 })
