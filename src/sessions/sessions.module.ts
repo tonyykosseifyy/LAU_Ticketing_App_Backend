fce0efa80cded92226488efcbef6ee603e7a5902
tonyykosseifyy <tonykosseify123@gmail.com>
2024-03-16 20:52:54 +0200

    add: attachUser function

--- src/sessions/sessions.module.ts
+++ src/sessions/sessions.module.ts
@@ -1,7 +1,12 @@
 import { Module } from '@nestjs/common';
 import { SessionsService } from './sessions.service';
+import { SessionSchema } from './schemas/session.schema';
+import { MongooseModule } from "@nestjs/mongoose"
+import { UsersModule } from 'src/users/users.module';
 
 @Module({
-  providers: [SessionsService]
+  imports: [MongooseModule.forFeature([{ name: "Session", schema: SessionSchema }]), UsersModule],
+  providers: [SessionsService],
+  exports: [SessionsService]
 })
 export class SessionsModule {}
--- src/sessions/sessions.service.ts
+++ src/sessions/sessions.service.ts
@@ -1,4 +1,38 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, BadRequestException, NotFoundException } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { ISession } from './interface/session.interface';
+import { Model, isValidObjectId } from 'mongoose';
+import { UsersService } from 'src/users/users.service';
 
 @Injectable()
-export class SessionsService {}
+export class SessionsService {
+    constructor(
+        @InjectModel('Session') private readonly sessionModel: Model<ISession>,
+        private readonly usersService: UsersService
+    ) {}
+
+    async attachUser(session_id: string, user_id: string) {
+        if (!isValidObjectId(session_id) || !isValidObjectId(user_id)) {
+            throw new BadRequestException('Invalid ID format');
+        }
+        const session = await this.sessionModel.findById(session_id);
+        
+        if (!session) {
+            throw new NotFoundException("Session not Found");
+        }
+        
+        const user = await this.usersService.findById(user_id);
+        
+        if (!user) {
+            throw new NotFoundException("User not Found");
+        }
+        try {
+            session.user_id = user_id;
+            await session.save();
+            return session;
+            
+        } catch (error) {
+            throw new BadRequestException(error);
+        }
+    }
+}
